466a1af3868091e6c8f8f32e4b3ad37c7b6b5782
Cristian Pattaro <cpattaro@gm.eurac.edu>
2024-04-03 06:34:29 +0200

    Remaining jobs fixed to get run in paralelle!
    Isuue was old snakemake version 7.34.1 which did not supprt executor: slurm setting in slurm/config.yaml file; instead used --cluster and --cluster-config slurm/config.yaml to introduce server settings!

--- workflow/run_pipeline.sh
+++ workflow/run_pipeline.sh
@@ -4,16 +4,16 @@
 #SBATCH --output %j_HAPLO_WF.log
 #SBATCH --partition batch
 #SBATCH --cpus-per-task 1
-#SBATCH --mem 128GB
-#SBATCH --time 8:00:00
+#SBATCH --mem 8GB
+#SBATCH --time 10-00:00:00
 
 export MAMBA_ROOT_PREFIX=/shared/Software/micromamba
 eval "$(micromamba shell hook --shell bash)"
 micromamba activate snakemake
 
-#snakemake --jobs 8  --reason --until get_dosage --default-resource mem_gb=8192  --latency-wait 10  --keep-going  --cluster 'sbatch  -p fast -c 8 --mem-per-cpu=8GB'
-snakemake --jobs 30 --profile slurm/   #--unlock
-
+#snakemake --jobs 30 --profile slurm/   #--unlock
+snakemake --jobs 30  --slurm  --cluster-config slurm/config.yaml
+1
 #micromamba deactivate
 
-#sbatch run_pipeline.txt -p fast
+#sbatch run_pipeline.sh
